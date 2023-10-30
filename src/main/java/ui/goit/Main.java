b75bce489ae52e9a2e77db5e88cc25a215b3c022
Igor Mikhaylichenko <igor.c.m@ukr.net>
2023-10-30 15:22:16 +0100

    Init

---
+++ src/main/java/ui/goit/Main.java
@@ -0,0 +1,13 @@
+package ui.goit;
+
+
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args) {
+       User user = new User("Ihor", "Mykhailychenko");
+       Gson gson = new Gson();
+
+       System.out.println(gson.toJson(user));
+    }
+}
---
+++ src/main/java/ui/goit/User.java
@@ -0,0 +1,3 @@
+package ui.goit;
+
+public record User(String name, String lastName) {}
